512b2112a214279997fca30c5d8baa34acfa95b4
Jasen Samuels <jasensamuels@gmail.com>
2021-12-24 13:37:06 -0500

    Update JM36_Elements.lua

--- JM36_Elements.lua
+++ JM36_Elements.lua
@@ -378,7 +378,7 @@ local function _init()
     --[[ Add Reload Option With Debug Mode ]]
     if DebugMode then
         local Scripts_Init = function()Scripts_Init()end -- Required as Stand errors and complains otherwise - function tables unsupported.
-		local menu = menu
+        local menu = menu
         menu.action(menu.my_root(), "Reload Modules", {"elements reload", "reload elements", "reload modules"}, "", Scripts_Init)
     end
     
