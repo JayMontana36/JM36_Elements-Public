0f191de8b8e8764db5bd8196535f994b9b7c2b6e
Jasen Samuels <jasensamuels@gmail.com>
2022-03-17 11:06:01 -0400

    Update GetHashKey.lua

--- store/JM36_Elements/__libs/GetHashKey.lua
+++ store/JM36_Elements/__libs/GetHashKey.lua
@@ -1,1 +1,16 @@
-GetHashKey = util.joaat
+local _GetHashKey = util.joaat
+local __GetHashKey = setmetatable({},
+	{
+		__mode = "kv",
+		__index	=	function(Table, Key)
+						local Hash = _GetHashKey(Key)
+						Table[Key] = Hash
+						return Hash
+					end,
+		__call	=	function(Table, String)
+						return Table[String]
+					end,
+	}
+)
+GetHashKey, util.joaat = __GetHashKey, __GetHashKey
+return __GetHashKey
