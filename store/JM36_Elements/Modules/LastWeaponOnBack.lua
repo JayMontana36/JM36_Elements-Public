895061146333e0ff058a9d480cb59ea5be8bfc44
Jasen Samuels <jasensamuels@gmail.com>
2022-03-17 23:50:11 -0400

    Update LastWeaponOnBack.lua

--- store/JM36_Elements/Modules/LastWeaponOnBack.lua
+++ store/JM36_Elements/Modules/LastWeaponOnBack.lua
@@ -6,17 +6,19 @@ local DoesEntityExist = DoesEntityExist
 local DeleteEntity = DeleteEntity
 local IsPedArmed = IsPedArmed
 local GetSelectedPedWeapon = GetSelectedPedWeapon
-local CreateObject = CreateObject
-local SetNetworkIdCanMigrate = SetNetworkIdCanMigrate
+--local CreateObject = CreateObject
+local SetEntityAsMissionEntity = SetEntityAsMissionEntity
+local SetEntityCleanupByEngine = SetEntityCleanupByEngine
 local NetworkGetNetworkIdFromEntity = NetworkGetNetworkIdFromEntity
+local SetNetworkIdCanMigrate = SetNetworkIdCanMigrate
 local AttachEntityToEntity = AttachEntityToEntity
---local SetEntityCleanupByEngine = SetEntityCleanupByEngine
 
 local menu = menu
 local memory = memory
 local memory_alloc = memory.alloc
 local memory_write_int = memory.write_int
 local memory_free = memory.free
+local entities_create_object = entities.create_object
 
 
 
@@ -37,18 +39,24 @@ local function RemoveCurrentWeaponEntityOnBack()
 	end
 end
 
-local Enabled, MenuOption
+local Enabled, MenuOption, DummyV3
 local WeaponsOnBackModule WeaponsOnBackModule = {
 	init	=	function()
 					do
 						local config = configFileRead("LastWeaponOnBack.ini")
 						Enabled = toboolean(config.Enabled)
+						if Enabled then DummyV3 = {x=0,y=0,z=0} end
 					end
 					
 					MenuOption = menu.toggle(menu.my_root(), "Last Weapon On Back", {}, "Puts your last used weapon on your back.", function(state)
-						Enabled = state
-						if not state then RemoveCurrentWeaponEntityOnBack() end
+						if state then
+							DummyV3 = {x=0,y=0,z=0}
+						else
+							RemoveCurrentWeaponEntityOnBack()
+							DummyV3 = nil
+						end
 						configFileWrite("LastWeaponOnBack.ini", {Enabled = state})
+						Enabled = state
 					end, Enabled)
 				end,
 	stop	=	function()
@@ -66,14 +74,20 @@ local WeaponsOnBackModule WeaponsOnBackModule = {
 						else
 							if LastWeaponEntityModel ~= CurrentWeaponEntityModelOnBack then
 								RemoveCurrentWeaponEntityOnBack()
-								CurrentWeaponEntityOnBack = CreateObject(LastWeaponEntityModel, 1.0, 1.0, 1.0, true, true, false)
-								SetNetworkIdCanMigrate(NetworkGetNetworkIdFromEntity(CurrentWeaponEntityOnBack), false)
+								--CurrentWeaponEntityOnBack = CreateObject(LastWeaponEntityModel, 1.0, 1.0, 1.0, true, true, false)
+								CurrentWeaponEntityOnBack = entities_create_object(LastWeaponEntityModel, DummyV3)
+								SetEntityAsMissionEntity(CurrentWeaponEntityOnBack, true, true)
+								SetEntityCleanupByEngine(CurrentWeaponEntityOnBack, true)
+								do
+									local NetworkId = NetworkGetNetworkIdFromEntity(CurrentWeaponEntityOnBack)
+									SetNetworkIdCanMigrate(CurrentWeaponEntityOnBack, false)
+									SetNetworkIdExistsOnAllMachines(CurrentWeaponEntityOnBack, true)
+								end
 								if not LastWeaponIsMeleeWeapon then
 									AttachEntityToEntity(CurrentWeaponEntityOnBack, Player_Ped, GetPedBoneIndex(Player_Ped, 24816), 0.075, -0.15, -0.02, 0.0, 165.0, 0.0, true, true, false, false, 2, true)
 								else
 									AttachEntityToEntity(CurrentWeaponEntityOnBack, Player_Ped, GetPedBoneIndex(Player_Ped, 24816), 0.11, -0.14, 0.0, -75.0, 185.0, 92.0, true, true, false, false, 2, true)
 								end
-								--SetEntityCleanupByEngine(CurrentWeaponEntityOnBack, false)
 							end
 						end
 					end
