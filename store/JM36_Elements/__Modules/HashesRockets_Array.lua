6dc4bc8710b9d61fa0e56b5b9fb95715715cf63e
Jasen Samuels <jasensamuels@gmail.com>
2022-04-15 12:20:02 -0400

    Create HashesRockets_Array.lua

---
+++ store/JM36_Elements/__Modules/HashesRockets_Array.lua
@@ -0,0 +1,20 @@
+local HashesRocketObjects = {
+	"w_lr_rpg_rocket",
+	"w_lr_homing_rocket",
+	"w_lr_firework_rocket",
+	"w_battle_airmissile_01",
+	"w_smug_airmissile_01b",
+	"w_ex_vehiclemissile_3",
+	"w_ex_vehiclemissile_1",
+	"w_ex_vehiclemissile_2",
+	"w_ex_vehiclemissile_4",
+	"w_smug_airmissile_02",
+}
+local HashesRocketObjectsNum = #HashesRocketObjects
+do
+	local GetHashKey = GetHashKey
+	for i=1, HashesRocketObjectsNum do
+		HashesRocketObjects[i] = GetHashKey(HashesRocketObjects[i])
+	end
+end
+return HashesRocketObjects, HashesRocketObjectsNum
