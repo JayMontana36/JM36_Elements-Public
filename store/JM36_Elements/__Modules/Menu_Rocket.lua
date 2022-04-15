08107dab53b686e21b4012c6c24537d18fab7be3
Jasen Samuels <jasensamuels@gmail.com>
2022-04-15 14:45:40 -0400

    Update Menu_Rocket.lua

--- store/JM36_Elements/__Modules/Menu_Rocket.lua
+++ store/JM36_Elements/__Modules/Menu_Rocket.lua
@@ -2,15 +2,14 @@ local menu = menu
 local Menu = menu.list(menu.my_root(), "Rocket Guidance", {}, "Rocket Related Options")
 do
 	local Enabled
-	do
-		local config = configFileRead("RocketGuidance.ini")
-		Enabled = toboolean(config.Enabled)
-	end
+	local config = configFileRead("RocketGuidance.ini")
+	Enabled = toboolean(config.Enabled)
 	local Info = Info
 	Info.RocketGuidanceEnabled = Enabled
-	menu.toggle(Menu, "Enable Rocket Guidance", {}, "Make rockets smarter | Increase rocket accuracy | Enable missile aimbot", function(state)
-		configFileWrite("RocketGuidance.ini", {Enabled = state})
+	menu.toggle(Menu, "Enable Rocket Guidance", {}, "Make rockets smarter\nIncrease rocket accuracy\nEnable missile aimbot", function(state)
 		Info.RocketGuidanceEnabled = state
+		config.Enabled = state
 	end, Enabled)
+	util.on_stop(function() configFileWrite("RocketGuidance.ini", config) end)
 end
 return Menu
