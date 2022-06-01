507a5be78b67f2aaa77596a1558ddf051d594475
Jasen Samuels <jasensamuels@gmail.com>
2022-06-01 09:59:40 -0400

    Update GetHashKey.lua

--- store/JM36_Elements/__libs/GetHashKey.lua
+++ store/JM36_Elements/__libs/GetHashKey.lua
@@ -1,3 +1,3 @@
-local _GetHashKey = require('CreateCacheSimpleForFunction')(util.joaat)
+local _GetHashKey <const> = require('CreateCacheSimpleForFunction')(util.joaat)
 GetHashKey, util.joaat = _GetHashKey, _GetHashKey
 return _GetHashKey
