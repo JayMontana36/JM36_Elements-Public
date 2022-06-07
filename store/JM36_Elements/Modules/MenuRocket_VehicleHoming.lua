354c67d51ef590dbd7c4209442e94a014c050583
Jasen Samuels <jasensamuels@gmail.com>
2022-06-07 04:56:13 -0400

    Update MenuRocket_VehicleHoming.lua

--- store/JM36_Elements/Modules/MenuRocket_VehicleHoming.lua
+++ store/JM36_Elements/Modules/MenuRocket_VehicleHoming.lua
@@ -1,63 +1,62 @@
-local MenuList
+local JM36 <const> = JM36
+local yield <const> = JM36.yield
+local CreateThread <const> = JM36.CreateThread
 
 local config
+CreateThread(function()
+	local RocketNearCoordGuideToEntity <const> = require'RocketNearCoordGuideToEntity'
+	local PtrMem <const> = require'Memory_SharedIntegerPointerSingle'
+	local memory_read_int <const> = memory.read_int
+	local CreateThread <const> = util.create_thread
+	local IsPedShooting <const> = IsPedShooting
+	local GetVehicleLockOnTarget <const> = GetVehicleLockOnTarget
+	local GetEntityCoords <const> = GetEntityCoords
+	local config <const> = config
+	local Info <const> = Info
+	local Player <const> = Info.Player
+	local Vehicle <const> = Player.Vehicle
+	local yield <const> = yield
+	while true do
+		if Info.RocketGuidanceEnabled then
+			if Vehicle.IsIn then
+				local Vehicle_Id <const> = Vehicle.Id
+				if IsPedShooting(Player.Ped) and GetVehicleLockOnTarget(Vehicle_Id, PtrMem) then
+					CreateThread(RocketNearCoordGuideToEntity, GetEntityCoords(Player_Ped, false), 25.0, memory_read_int(PtrMem), config.Realistic, config.Accuracy)
+				end
+			end
+		end
+		yield()
+	end
+end)
 
-local memory = memory
-
-local Accuracy, Realistic
-
-local RocketNearCoordGuideToEntity = require'RocketNearCoordGuideToEntity'
-local util_create_thread = util.create_thread
-local memory_read_int = memory.read_int
-
-local GetVehicleLockOnTarget, IsPedShooting
-	= GetVehicleLockOnTarget, IsPedShooting
-
-local Mem
-return{
+local Menu
+return
+{
 	init	=	function()
-					Mem = memory.alloc()
+					local menu <const> = menu
 					
-					local Menu = require'Menu_Rocket'
-					local menu = menu
-					MenuList = menu.list(Menu, "Vehicle Homing Rocket Tuning", {}, "")
-					local MenuList = MenuList
+					Menu = menu.list(require'Menu_Rocket', "Vehicle Homing Rocket Tuning", {}, "")
+					local Menu <const> = Menu
 					
 					do
-						config = configFileRead("RocketGuidance_VehicleHoming.ini")
-						local config = config
-						Accuracy = tonumber(config.Accuracy or 1)
+						config = configFileRead("RocketGuidance_VehicleHoming.ini") local config <const> = config
+						config.Accuracy = tonumber(config.Accuracy or 1)
 						if config.Realistic == nil then
-							Realistic = true
+							config.Realistic = true
 						else
-							Realistic = toboolean(config.Realistic)
+							config.Realistic = toboolean(config.Realistic)
 						end
 					end
 					
-					menu.slider(MenuList, "Guidance Accuracy Multiplier", {}, "Rocket Guidance Accuracy Multiplier For Vehicle Homing Rockets", 1, 10, Accuracy, 1, function(value, prev_value, click_type) 
-						Accuracy = value
+					menu.slider(Menu, "Guidance Accuracy Multiplier", {}, "Rocket Guidance Accuracy Multiplier For Vehicle Homing Rockets", 1, 10, config.Accuracy, 1, function(value, prev_value, click_type)
 						config.Accuracy = value
 					end)
-					menu.toggle(MenuList, "Guidance Uses Realistic Physics", {}, "Rocket Guidance Uses Realistic Physics For Vehicle Homing Rockets", function(state)
-						Realistic = state
+					menu.toggle(Menu, "Guidance Uses Realistic Physics", {}, "Rocket Guidance Uses Realistic Physics For Vehicle Homing Rockets", function(state)
 						config.Realistic = state
-					end, Realistic)
+					end, config.Realistic)
 				end,
 	stop	=	function()
 					configFileWrite("RocketGuidance_VehicleHoming.ini", config)
-					menu.delete(MenuList)
-					memory.free(Mem)
-				end,
-	loop	=	function(Info)
-					if Info.RocketGuidanceEnabled then
-						local Player = Info.Player
-						local Vehicle = Player.Vehicle
-						if Vehicle.IsIn then
-							local Player_Ped = Player.Ped
-							if IsPedShooting(Player_Ped) and GetVehicleLockOnTarget(Vehicle.Id, Mem) then
-								util_create_thread(RocketNearCoordGuideToEntity, GetEntityCoords(Player_Ped, false), 25.0, memory_read_int(Mem), Realistic, Accuracy)
-							end
-						end
-					end
+					menu.delete(Menu)
 				end,
 }
