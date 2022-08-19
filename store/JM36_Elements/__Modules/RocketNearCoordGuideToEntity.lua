66e8d361df46112492650b0401f66da02cd36ddf
Jasen Samuels <jasensamuels@gmail.com>
2022-08-19 00:32:35 -0400

    Update RocketNearCoordGuideToEntity.lua

--- store/JM36_Elements/__Modules/RocketNearCoordGuideToEntity.lua
+++ store/JM36_Elements/__Modules/RocketNearCoordGuideToEntity.lua
@@ -30,6 +30,10 @@ return function(CoordsRocket, CoordsRadius, TargetEntity, UseRealisticPhysics, G
 		local SpeedInit
 		local GuidanceAccuracy <const> = (GuidanceAccuracy or 1) * 32
 		local TargetEntityIsVehicle <const> = GetEntityType(TargetEntity) == 2
+		--CreateThread(function()
+		--	NetworkUnregisterNetworkedEntity(Rocket)
+		--	NetworkRegisterEntityAsNetworked(Rocket)
+		--end)
 		do
 			local NetId = ObjToNet(Rocket)
 			if NetId == 0 or NetId == -1 then
@@ -55,45 +59,50 @@ return function(CoordsRocket, CoordsRadius, TargetEntity, UseRealisticPhysics, G
 			
 			CoordsRocket = GetEntityCoords(Rocket, false)
 			local CoordsTarget <const> = GetEntityCoords(TargetEntity, false)
-			
-			local Rotation = util.v3_look_at(CoordsRocket, CoordsTarget)
-			
-			SetEntityRotation(Rocket, Rotation.x, Rotation.y, Rotation.z, 2, false)
-			
-			if not SpeedInit then
-				SetEntityMaxSpeed(Rocket, 158.2) -- Max Recorded Speed = 158.10801696777
-				SpeedInit = 1
-			end
-			
-			Rotation = util.rot_to_dir(Rotation)
-			
-			local ApplyForceToEntityCenterOfMass <const> = ApplyForceToEntityCenterOfMass
-			if UseRealisticPhysics then
-				ApplyForceToEntityCenterOfMass(Rocket, 1, Rotation.x*GuidanceAccuracy, Rotation.y*GuidanceAccuracy, Rotation.z*GuidanceAccuracy, false, false, true, true)
-			else
-				for i=1, GuidanceAccuracy do
-					ApplyForceToEntityCenterOfMass(Rocket, 1, Rotation.x, Rotation.y, Rotation.z, false, false, true, true)
+	--		if HasEntityClearLosToEntityInFront(Rocket, TargetEntity) then
+				local Rotation = util.v3_look_at(CoordsRocket, CoordsTarget)
+				
+				SetEntityRotation(Rocket, Rotation.x, Rotation.y, Rotation.z, 2, false)
+				
+				if not SpeedInit then
+					SetEntityMaxSpeed(Rocket, 158.2) -- Max Recorded Speed = 158.10801696777
+					SpeedInit = 1
 				end
-			end
-			
-			--[[do
-				UseParticleFxAsset(PtFxAssName)
-				SetParticleFxNonLoopedColour(1.0, 0, 0)
-				StartNetworkedParticleFxNonLoopedOnEntity(PtFxEffName, Rocket, 0.0, 0.0, 0.0, 0.0, 0.0, 180.0, Scale, false, false, false)
-			end]]
-			
-			DrawLine(CoordsRocket.x, CoordsRocket.y, CoordsRocket.z, CoordsTarget.x, CoordsTarget.y, CoordsTarget.z, 255, 255, 255, 255)
-			
-			if TargetEntityIsVehicle then
-				_0x407DC5E97DB1A4D3(TargetEntity, 2)
-			end
+				
+				Rotation = util.rot_to_dir(Rotation)
+				
+				local ApplyForceToEntityCenterOfMass <const> = ApplyForceToEntityCenterOfMass
+				if UseRealisticPhysics then
+					ApplyForceToEntityCenterOfMass(Rocket, 1, Rotation.x*GuidanceAccuracy, Rotation.y*GuidanceAccuracy, Rotation.z*GuidanceAccuracy, false, false, true, true)
+				else
+					for i=1, GuidanceAccuracy do
+						ApplyForceToEntityCenterOfMass(Rocket, 1, Rotation.x, Rotation.y, Rotation.z, false, false, true, true)
+					end
+				end
+				
+				--[[do
+					UseParticleFxAsset(PtFxAssName)
+					SetParticleFxNonLoopedColour(1.0, 0, 0)
+					StartNetworkedParticleFxNonLoopedOnEntity(PtFxEffName, Rocket, 0.0, 0.0, 0.0, 0.0, 0.0, 180.0, Scale, false, false, false)
+				end]]
+				
+				DrawLine(CoordsRocket.x, CoordsRocket.y, CoordsRocket.z, CoordsTarget.x, CoordsTarget.y, CoordsTarget.z, 255, 255, 255, 255)
+				
+				if TargetEntityIsVehicle then
+					_0x407DC5E97DB1A4D3(TargetEntity, 2)
+				end
+				
+				
+	--		end
 			
 			util_yield()
+			
+--			StopParticleFxLooped(ptfx, false)
+--			RemoveParticleFx(ptfx, false)
+			
+			if CoordsRocket and not DoesEntityExist(Rocket) then
+				AddOwnedExplosion(PlayerPedId(), CoordsRocket.x, CoordsRocket.y, CoordsRocket.z, --[[63]]--[[64]]--[[70]]64, 1.0, false, true, 10.0)
+			end
 		end
-		if CoordsRocket and not DoesEntityExist(Rocket) then
-			AddOwnedExplosion(PlayerPedId(), CoordsRocket.x, CoordsRocket.y, CoordsRocket.z, --[[63]]--[[64]]--[[70]]64, 1.0, false, true, 10.0)
-		end
---		StopParticleFxLooped(ptfx, false)
---		RemoveParticleFx(ptfx, false)
 	end
 end
