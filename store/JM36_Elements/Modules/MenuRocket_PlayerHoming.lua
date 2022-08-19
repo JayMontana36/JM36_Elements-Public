15fd7043486562ffb3a6ba269fcc8df6f687650e
Jasen Samuels <jasensamuels@gmail.com>
2022-08-19 00:36:03 -0400

    Update MenuRocket_PlayerHoming.lua

--- store/JM36_Elements/Modules/MenuRocket_PlayerHoming.lua
+++ store/JM36_Elements/Modules/MenuRocket_PlayerHoming.lua
@@ -25,6 +25,71 @@ do
 	util_yield = util.yield
 end
 
+local CreateThread <const> = JM36.CreateThread
+CreateThread(function()
+	local memory_read_int <const> = memory.read_int
+	local MemPtr = require'Memory_SharedIntegerPointerSingle'
+	
+	local Info <const> = Info
+	local Player <const> = Info.Player
+	local Vehicle <const> = Player.Vehicle
+	local TargetTable <const> = Info.Target
+	local Rockets <const> = Info.Rockets
+	
+	--local Radius <const> = 1.5
+	local Radius <const> = 5.0
+	local GuidanceAccuracy <const> = 1
+	local UseRealisticPhysics <const> = true
+	
+	local yield <const> = JM36.yield
+	while true do
+		if not Vehicle.IsIn then
+			local Player_Ped = Player.Ped
+			if WeaponHashes[GetSelectedPedWeapon(Player_Ped)] then
+				--HideHudComponentThisFrame(14)
+				
+				local Target = TargetTable.CollisionA and TargetTable.EntityHitA or TargetTable.CollisionB and TargetTable.EntityHitB
+				
+				local Player_Id <const> = Player.Id
+				--SetPlayerHomingRocketDisabled(Player_Id, true)
+				
+				if IsPlayerFreeAiming(Player_Id) and Target and Target ~= 0 then
+					local CamRot <const> = GetFinalRenderedCamRot(2)
+					local Coords <const> = GetEntityCoords(Target, true)
+					DrawMarker(3,Coords.x,Coords.y,Coords.z,0.0,0.0,0.0,CamRot.x,CamRot.y,0.0,5.0,5.0,5.0,255,255,0,255,false,true,0,false,"helicopterhud","hud_dest",false)
+				end
+				if IsPedShooting(Player_Ped) then
+					CreateThread(function()
+						local Rocket
+						for i=1, #Rockets do
+							local RocketTable <const> = Rockets[i]
+							if RocketTable[2] < Radius and not IsEntityAMissionEntity(RocketTable[1]) then
+								Rocket = RocketTable
+								break
+							end
+						end
+						
+						if Rocket then
+							local _Rocket <const> = Rocket[1]
+							while DoesEntityExist(_Rocket) and (not Target or Target == 0) do
+								yield()
+								Target = TargetTable.CollisionA and TargetTable.EntityHitA or TargetTable.CollisionB and TargetTable.EntityHitB
+								if (not Target or Target == 0) and GetPlayerTargetEntity(Player_Id, MemPtr) then
+									Target = memory_read_int(MemPtr)
+								end
+							end
+							if DoesEntityExist(_Rocket) and (Target and Target ~= 0) then
+								RocketNearCoordGuideToEntity(Rocket[3], Radius, Target, UseRealisticPhysics, GuidanceAccuracy)
+							end
+						end
+					end)
+				end
+			end
+		end
+		yield()
+	end
+end)
+
 return{
 	init	=	function()
 					local Menu = require'Menu_Rocket'
@@ -55,53 +120,5 @@ return{
 	stop	=	function()
 					configFileWrite("RocketGuidance_Player.ini", config)
 					menu.delete(MenuList)
-				end,
-	loop	=	function(Info)
-					if Info.RocketGuidanceEnabled then
-						local Player = Info.Player
-						local Vehicle = Player.Vehicle
-						if not Vehicle.IsIn then
-							local Player_Ped = Player.Ped
-							if WeaponHashes[GetSelectedPedWeapon(Player_Ped)] then
-								--HideHudComponentThisFrame(14)
-								
-								local TargetTable = Info.Target
-								local Target = TargetTable.CollisionA and TargetTable.EntityHitA or TargetTable.CollisionB and TargetTable.EntityHitB
-								
-								local Player_Id = Player.Id
-								--SetPlayerHomingRocketDisabled(Player_Id, true)
-								
-								if IsPlayerFreeAiming(Player_Id) then
-									if Target and Target ~= 0 then
-										local CamRot = GetFinalRenderedCamRot(2)
-										local Coords = GetEntityCoords(Target, true)
-										DrawMarker(3,Coords.x,Coords.y,Coords.z,0.0,0.0,0.0,CamRot.x,CamRot.y,0.0,5.0,5.0,5.0,255,255,0,255,false,true,0,false,"helicopterhud","hud_dest",false)
-									end
-								end
-								if IsPedShooting(Player_Ped) then
-									util_create_thread(function()
-										local PlayerCoords = GetEntityCoords(Player_Ped, false)
-										local Rocket = 0
-										for i=1, 3 do
-											Rocket = GetClosestObjectOfType(PlayerCoords.x, PlayerCoords.y, PlayerCoords.z, 1.5, RocketObjectHashes[i], false)
-											if Rocket ~= 0 then break end
-										end
-										
-										while DoesEntityExist(Rocket) and (not Target or Target == 0) do
-											Target = TargetTable.CollisionA and TargetTable.EntityHitA or TargetTable.CollisionB and TargetTable.EntityHitB
-											
-											if not Target or Target == 0 then
-												util_yield()
-											end
-										end
-										
-										if DoesEntityExist(Rocket) and (Target and Target ~= 0) then
-											RocketNearCoordGuideToEntity(GetEntityCoords(Rocket, false), 1.5, Target, UseRealisticPhysics, GuidanceAccuracy)
-										end
-									end)
-								end
-							end
-						end
-					end
 				end,
 }
