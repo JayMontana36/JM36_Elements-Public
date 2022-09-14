2e66b85c8e76ae927ff74087977d6220729ead8d
Jasen Samuels <jasensamuels@gmail.com>
2022-09-14 14:40:03 -0400

    Update SetVehicleExclusiveDriver.lua

--- store/JM36_Elements/__Modules/SetVehicleExclusiveDriver.lua
+++ store/JM36_Elements/__Modules/SetVehicleExclusiveDriver.lua
@@ -3,12 +3,8 @@ local SetNetworkIdCanMigrate <const> = SetNetworkIdCanMigrate
 local SetVehicleExclusiveDriver <const> = SetVehicleExclusiveDriver
 local _0x41062318F23ED854 <const> = _0x41062318F23ED854
 
-return function(HandleVehEnt, HandleVehNet, State)
+return function(HandleVehEnt, HandleVehNet, State, Ped)
 	SetNetworkIdCanMigrate(HandleVehNet, not State)
-	if State then
-		SetVehicleExclusiveDriver(HandleVehEnt, Player.Ped, 1)
-	else
-		SetVehicleExclusiveDriver(HandleVehEnt, 0, 1)
-	end
+	SetVehicleExclusiveDriver(HandleVehEnt, State and (Ped or Player.Ped) or 0, 1)
 	_0x41062318F23ED854(HandleVehEnt, State)
 end
