8bc03dac8b60035ada1a4dbd8f111239d9208678
Jasen Samuels <jasensamuels@gmail.com>
2022-09-20 13:46:35 -0400

    Update Memory_SharedIntegerPointerSingle.lua

--- store/JM36_Elements/__Modules/Memory_SharedIntegerPointerSingle.lua
+++ store/JM36_Elements/__Modules/Memory_SharedIntegerPointerSingle.lua
@@ -1,1 +1,10 @@
-return memory.alloc_int()
+local SharedIntPtr = memory.alloc_int()
+JM36.CreateThread_HighPriority(function()
+	local memory_write_int = memory.write_int
+	local yield = JM36.yield
+	while true do
+		memory_write_int(SharedIntPtr, 0)
+		yield()
+	end
+end)
+return SharedIntPtr
